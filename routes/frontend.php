b9c63d810eba8db64af44df0a1d67430f4e723ae
Jimmy <toluadejimi@gmail.com>
2023-06-05 16:07:38 +0100

    gjjgjjg

--- routes/frontend.php
+++ routes/frontend.php
@@ -1,4 +1,5 @@
 <?php 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AUTH\RegisterController;
 use App\Http\Controllers\Frontend as FRONTEND;
@@ -6,10 +7,9 @@ use App\Http\Controllers\Frontend as FRONTEND;
 
 
 Auth::routes();
-
 Route::get('/',  			        [FRONTEND\HomeController::class,'index']);
 Route::get('/',  			        [FRONTEND\HomeController::class,'index']);
-Route::post('/register-user', 		    [RegisterController::class,'create']);
+Route::post('/register-user', 		    [AUTH\RegisterController::class,'create']);
 
 
 
