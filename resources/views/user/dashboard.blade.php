94a1421b510cea8a1bc3188c35eef56c75e08b84
Jimmy <toluadejimi@gmail.com>
2023-06-12 22:46:12 +0100

    up

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -95,20 +95,20 @@
 
 
               <p>Fund your account instantly</p>
-<br>
+
 
               <!-- <button style="color:white; border:none; border-radius:20px; padding: 8px; background-color: red;"  type="button">  <a style="color: #E5F7FE;" class="nav-link {{ Request::is('user/rules*') ? 'active' : '' }}" href="{{ url('https://oprime.com.ng/wordpress/our-rules') }}">
       <i class="fi fi-rs-paper-plane"></i>
       <span class="nav-link-text">{{ __('CLICK ME TO READ MY RULES DONT SAY I DIDNT TELL YOU') }}</span>
     </a></button>
-<br>
-<br>
+
+
 <marquee>              <button style="color:white; border:none; border-radius:20px; padding: 8px; background-color: red;"  type="button">  <a style="color: #E5F7FE;" class="nav-link {{ Request::is('user/rules*') ? 'active' : '' }}" href="{{ url('https://oprime.com.ng/wordpress/our-rules') }}">
       <i class="fi fi-rs-paper-plane"></i>
       <span class="nav-link-text">{{ __('CLICK ME TO READ MY RULES DONT SAY I DIDNT TELL YOU') }}</span>
     </a></button></marquee> -->
 
-<br>
+
               </div>
             
             
