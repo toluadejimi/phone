dcd87050e2f8d90e643eedadcf30162e7cf97818
Jimmy <toluadejimi@gmail.com>
2023-06-19 11:39:44 +0100

    up

--- resources/views/layouts/main/footer.blade.php
+++ resources/views/layouts/main/footer.blade.php
@@ -6,7 +6,7 @@
             call_to_action: "Message us", // Call to action
             button_color: "#129BF4", // Color of button
             position: "right", // Position may be 'right' or 'left'
-            order: "facebook,whatsapp", // Order of buttons
+            order: "whatsapp", // Order of buttons
         };
         var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
         var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
